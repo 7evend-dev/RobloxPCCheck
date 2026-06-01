63bc34abcd23acb02bda295ec03801fda7f908eb
7evend <evendakame@gmail.com>
2026-06-01 12:49:09 +0300

    Checker.ps1

--- Checker.ps1
+++ Checker.ps1
@@ -1,15 +1,3 @@
-echo.
-echo.
-echo      8888888888  8888888888  888     888  8888888888  888b    888  8888888b.  
-echo            d88P  888         888     888  888         8888b   888  888  "Y88b 
-echo           d88P   888         888     888  888         88888b  888  888    888 
-echo          d88P    8888888     Y88b   d88P  8888888     888Y88b 888  888    888 
-echo         8888     888          Y88b d88P   888         888 Y88b888  888    888 
-echo        d88P      888           Y88o88P    888         888  Y88888  888    888  
-echo       d88P       8888888888      Y8P      8888888888  888    Y888  8888888P"  
-echo.
-echo.
-
 $configJson = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/7evend-dev/RobloxPCCheck/refs/heads/main/cfg.json" 
 $Solara = $configJson.Solara
 $EntryPoint = $configJson.EntryPoint
